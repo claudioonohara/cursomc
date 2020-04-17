7d3eb2966984cbc6cb776e2648efa4c3dfe4b607
Claudio Toyoshi Onohara <claudioonohara@gmail.com>
2020-04-17 07:33:28 -0400

    Ajustes no endpoint /categorias/{id}

--- src/main/java/br/eti/claudioonohara/cursomc/services/CategoriaService.java
+++ src/main/java/br/eti/claudioonohara/cursomc/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import br.eti.claudioonohara.cursomc.domain.Categoria;
 import br.eti.claudioonohara.cursomc.repositories.CategoriaRepository;
+import br.eti.claudioonohara.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo:" + Categoria.class.getName()));
 	}
 }

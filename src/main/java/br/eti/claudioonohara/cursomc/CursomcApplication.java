bbde6b1385b595a5a542443b7c8a6bc01befb99e
Claudio Toyoshi Onohara <claudioonohara@gmail.com>
2020-04-20 07:29:53 -0400

    ItemPedido e ItemPedidoPK

--- src/main/java/br/eti/claudioonohara/cursomc/CursomcApplication.java
+++ src/main/java/br/eti/claudioonohara/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import br.eti.claudioonohara.cursomc.domain.Cidade;
 import br.eti.claudioonohara.cursomc.domain.Cliente;
 import br.eti.claudioonohara.cursomc.domain.Endereco;
 import br.eti.claudioonohara.cursomc.domain.Estado;
+import br.eti.claudioonohara.cursomc.domain.ItemPedido;
 import br.eti.claudioonohara.cursomc.domain.Pagamento;
 import br.eti.claudioonohara.cursomc.domain.PagamentoComBoleto;
 import br.eti.claudioonohara.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import br.eti.claudioonohara.cursomc.repositories.CidadeRepository;
 import br.eti.claudioonohara.cursomc.repositories.ClienteRepository;
 import br.eti.claudioonohara.cursomc.repositories.EnderecoRepository;
 import br.eti.claudioonohara.cursomc.repositories.EstadoRepository;
+import br.eti.claudioonohara.cursomc.repositories.ItemPedidoRepository;
 import br.eti.claudioonohara.cursomc.repositories.PagamentoRepository;
 import br.eti.claudioonohara.cursomc.repositories.PedidoRepository;
 import br.eti.claudioonohara.cursomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner{
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -114,6 +118,19 @@ public class CursomcApplication implements CommandLineRunner{
 				
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 
 }
